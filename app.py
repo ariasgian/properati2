a3935eab16ae2eeeb2984ee7d7ebced3d052871e
ariasgian <ariasgian@gmail.com>
2021-07-31 13:57:04 -0300

    se agrega configuracion versel

--- app.py
+++ app.py
@@ -62,7 +62,12 @@ def scrapeo(barrio,inmueble,tipo, limite):
 
 
 
-app.run( port = 3030, host = '0.0.0.0' )
+
+if __name__== "__main__":
+    if FLASK_ENV == "development":
+        app.run( port = 3030, host='0.0.0.0' )
+    else:
+        app.run( port= 3030 )
 
 
 
@@ -71,49 +76,3 @@ app.run( port = 3030, host = '0.0.0.0' )
 
 
 
-
-
-# num = 1    
-# barrio= 'palermo'
-# inmueble= 'departamento'
-# tipo = 'alquiler'
-
-
-
-
-# fin, dom = pagina(1, barrio, inmueble, tipo)
-# #print(fin.text)
-# id = 1
-# propiedad =[]
-
-# while fin != None:
-    
-#     anuncios = dom.find_all( attrs = { 'class' : 'StyledCard-n9541a-1 jWSYcc' } )
-    
-    
-#     for anuncio in anuncios:
-                
-#         titulo = anuncio.find( attrs = { 'class' : 'StyledTitle-n9541a-4 bwJAej' } )
-#         precio = anuncio.find( attrs = { 'class' : 'StyledPrice-sc-1wixp9h-0 bZCCaW' } )
-#         expensas = anuncio.find( attrs = { 'class' : 'StyledMaintenanceFees-n9541a-6 cRsmn' } )
-#         detalles = anuncio.find( attrs = { 'class' : 'StyledInfoIcons-n9541a-9 fgcFIO' } )
-#         inmobiliaria = anuncio.find( attrs = { 'class' : 'seller-name' } )
-        
-#         if titulo: titulo = titulo.get_text()
-#         if precio: precio= precio.get_text()
-#         if expensas: expensas = unicodedata.normalize("NFKD", expensas.get_text()) 
-#         if inmobiliaria: imobiliaria = inmobiliaria.get_text()
-#         if detalles:
-#             spans = detalles.find_all('span')
-#             for span in spans:
-#                 txt = span.get_text()
-#                 if (txt.find('m²')>=0): m2 = txt
-#                 if (txt.find('ambiente')>=0): ambientes = txt
-#                 if (txt.find('baño')>=0): banios = txt
-#         valor = {'id': id, 'precio': precio, 'expensas': expensas, 'm2': m2}
-#         propiedad.append(valor)
-#         id= id+1
-#     #print(num, fin)
-#     num= num + 1            
-#     fin, dom= pagina(num, barrio, inmueble, tipo)
-    
